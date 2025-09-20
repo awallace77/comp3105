31a384e97fa32f336c4eba2134e6618c531d85e3
awallace77 <andrew77wallace@outlook.com>
2025-09-20 12:07:42 -0400

    Working on q1 part c

--- a1/A1codes.py
+++ a1/A1codes.py
@@ -31,21 +31,21 @@ def minimizeLinf(X, y):
 
     # Objective coefficients
     c = np.concatenate([np.zeros([d, 1]), np.ones([1,1])])
-    print("Printing c:")
-    print(c, c.shape, "\n")
+    # print("Printing c:")
+    # print(c, c.shape, "\n")
 
     # Technology matrix
     G_1 = np.concatenate([np.zeros([1, d]), -np.ones([1,1])], axis=1)
     G_2 = np.concatenate([X, -(np.ones([n, 1]))], axis=1)
     G_3 = np.concatenate([-X, -(np.ones([n, 1]))], axis=1)
     G = np.concatenate([G_1, G_2, G_3], axis=0)
-    print("Printing G:")
-    print(G, G.shape, "\n")
+    # print("Printing G:")
+    # print(G, G.shape, "\n")
 
     # Right hand side vector
     h = np.concatenate([np.zeros((1, 1)), y, -y], axis=0)
-    print("Printing h:")
-    print(h, h.shape, "\n")
+    # print("Printing h:")
+    # print(h, h.shape, "\n")
 
     # Convert to solver matrix
     c_final = matrix(c)
@@ -53,14 +53,120 @@ def minimizeLinf(X, y):
     h_final = matrix(h)
 
     # Solve for unknowns
+    solvers.options['show_progress'] = False
     sol = solvers.lp(c_final, G_final, h_final)
+
+    # Return only values for w and do not include threshold \delta
     final_sol = np.array(sol['x'])[:-1, :]
-    print(np.array(sol['x']))
-    print(final_sol)
+    # print(np.array(sol['x']))
+    # print(final_sol)
     return final_sol
 
+'''
+    synRegExperiments
+    Output: A 2x2 matrix of average traning losses and a 2x2 matrix of average test losses for L2 & Linf models and L2 & Linf losses
+'''
+def synRegExperiments():
+    def genData(n_points, is_training=False):
+        '''
+        This function generate synthetic data
+        '''
+        X = np.random.randn(n_points, d) # input matrix
+        X = np.concatenate((np.ones((n_points, 1)), X), axis=1) # augment input
+        y = X @ w_true + np.random.randn(n_points, 1) * noise # ground truth label
+        if is_training:
+            y[0] *= -0.1
+        return X, y
 
-# if __name__ == "__main__":
+    n_runs = 100
+    n_train = 30
+    n_test = 1000
+    d = 5
+    noise = 0.2
+    train_loss = np.zeros([n_runs, 2, 2]) # n_runs * n_models * n_metrics
+    test_loss = np.zeros([n_runs, 2, 2]) # n_runs * n_models * n_metrics
+    
+    # DONE: Change the following random seed to one of your student IDs
+    np.random.seed(101210291)
+    for r in range(n_runs):
+        w_true = np.random.randn(d + 1, 1)
+        Xtrain, ytrain = genData(n_train, is_training=True)
+        Xtest, ytest = genData(n_test, is_training=False)
+        w_L2 = minimizeL2(Xtrain, ytrain)
+        w_Linf = minimizeLinf(Xtrain, ytrain)
+
+        # DONE: Evaluate the two models' performance (for each model,
+        # calculate the L2 and L infinity losses on the training
+        # data). Save them to `train_loss`
+
+        # TRAINING DATA
+            # L2 model
+        L2model_L2_loss = np.mean(((Xtrain @ w_L2) - ytrain) ** 2) 
+        L2model_Linf_loss = np.max(np.abs((Xtrain @ w_L2) - ytrain))
+        train_loss[r][0][0] = L2model_L2_loss
+        train_loss[r][0][1] = L2model_Linf_loss
+            # L_inf model
+        Linfmodel_L2_loss = np.mean(((Xtrain @ w_Linf) - ytrain) ** 2) 
+        Linfmodel_Linf_loss = np.max(np.abs((Xtrain @ w_Linf) - ytrain))
+        train_loss[r][1][0] = Linfmodel_L2_loss
+        train_loss[r][1][1] = Linfmodel_Linf_loss
+
+        # DONE: Evaluate the two models' performance (for each model,
+        # calculate the L2 and L infinity losses on the test
+        # data). Save them to `test_loss`
+        # TEST DATA
+            # L2 model
+        L2model_L2_loss = np.mean(((Xtest @ w_L2) - ytest) ** 2) 
+        L2model_Linf_loss = np.max(np.abs((Xtest @ w_L2) - ytest))
+        test_loss[r][0][0] = L2model_L2_loss
+        test_loss[r][0][1] = L2model_Linf_loss
+            # L_inf model
+        Linfmodel_L2_loss = np.mean(((Xtest@ w_Linf) - ytest) ** 2) 
+        Linfmodel_Linf_loss = np.max(np.abs((Xtest @ w_Linf) - ytest))
+        test_loss[r][1][0] = Linfmodel_L2_loss
+        test_loss[r][1][1] = Linfmodel_Linf_loss
+
+    # DONE: compute the average losses over runs
+    # TRAINING DATA - AVG TRAIN LOSS
+    avg_train_loss = np.zeros([2,2])
+
+    # Total L2 model - L2 losses and Linf losses
+    total_L2model_L2_loss = np.sum(train_loss[:, 0, 0]) 
+    total_L2model_Linf_loss = np.sum(train_loss[:, 0, 1]) 
+
+    # Total Linf model - L2 losses and Linf losses
+    total_Linfmodel_L2_loss = np.sum(train_loss[:, 1, 0]) 
+    total_Linfmodel_Linf_loss = np.sum(train_loss[:, 1, 1]) 
+    
+    avg_train_loss[0][0] = total_L2model_L2_loss / n_runs     # Avg L2 model L2 loss
+    avg_train_loss[0][1] = total_L2model_Linf_loss / n_runs   # Avg L2 model Linf loss 
+    avg_train_loss[1][0] = total_Linfmodel_L2_loss / n_runs   # Avg Linf model L2 loss
+    avg_train_loss[1][1] = total_Linfmodel_Linf_loss / n_runs # Avg Linf model Linf loss
+    print(avg_train_loss)
+    
+    # TEST DATA - AVG TEST LOSS
+    avg_test_loss = np.zeros([2,2])
+    
+    # Total L2model L2 losses and Linf losses
+    total_L2model_L2_loss = np.sum(test_loss[:, 0, 0]) 
+    total_L2model_Linf_loss = np.sum(test_loss[:, 0, 1]) 
+
+    # Total Linf Model L2 losses and Linf losses
+    total_Linfmodel_L2_loss = np.sum(test_loss[:, 1, 0]) 
+    total_Linfmodel_Linf_loss = np.sum(test_loss[:, 1, 1]) 
+    
+    avg_test_loss[0][0] = total_L2model_L2_loss / n_runs     # Avg L2 model L2 loss
+    avg_test_loss[0][1] = total_L2model_Linf_loss / n_runs   # Avg L2 model Linf loss 
+    avg_test_loss[1][0] = total_Linfmodel_L2_loss / n_runs   # Avg Linf model L2 loss
+    avg_test_loss[1][1] = total_Linfmodel_Linf_loss / n_runs # Avg Linf model Linf loss
+    print(avg_test_loss)
+    
+    # DONE: return a 2-by-2 training loss variable and a 2-by-2 test loss variable
+    return avg_train_loss, avg_test_loss
+
+
+if __name__ == "__main__":
     # X = np.array([[1, 2, 3], [1, 4, 5], [1, 6, 7]])
     # y = np.array([1, 2, 3])
     # minimizeLinf(X, y)
+    synRegExperiments()
