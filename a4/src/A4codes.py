a825bcac4cc6e4a92bcfc522c2b9c511784695e3
awallace77 <andrew77wallace@outlook.com>
2025-11-22 12:18:51 -0500

    a4 setup

---
+++ a4/src/A4codes.py
@@ -0,0 +1,9 @@
+
+'''
+    COMP3105 Intro to Machine Learning
+    Assignment 4
+    Group 51:
+        Andrew Wallace - 101210291
+        Christer Henrysson - 101260693
+    Due: November 30th, 2025
+'''
